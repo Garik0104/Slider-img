6b964e1b66449edd95856708f4bc5015ddefe0b6
Игорь <114926820+Garik0104@users.noreply.github.com>
2023-05-02 12:17:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const upBtn = document.querySelector('.up-button')
+
+const downbtn = document.querySelector('.down-button')
+
+const sidebar = document.querySelector('.sidebar')
+
+const mainSlide = document.querySelector('.main-slide')
+
+const container = document.querySelector('.container')
+
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click',() => {
+    changesSlide('up')
+} )
+
+downbtn.addEventListener('сlick',() => {
+    changesSlide('down')
+})
+
+function changesSlide(direction) {
+ if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount)
+    {
+    activeSlideIndex = 0
+    }
+ } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+        activeSlideIndex = slidesCount -1
+    }
+ }
+
+ const height = container.clientHeight
+
+ mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+ sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+
+}
